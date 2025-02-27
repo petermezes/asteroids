15a4cdd1e1afa294ffc109a3a419c6de81e63670
petermezes <peter.mezes@hehex.sk>
2025-02-27 18:48:04 +0100

    player on screen and controls added

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import pygame
 from constants import *
+from player import Player
 
 game_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+fps = 60
 
-def main():
+
+def init_log():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
+
+def main():
+    init_log()
     pygame.init()
-    
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
+        # loop head
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(game_window, (0,0,0))
+
+        # loop body
+        pygame.Surface.fill(game_window, (0, 0, 0))
+        player.draw(game_window)
+        player.update(dt)
+
+        # loop tail
         pygame.display.flip()
-        
-    
+        dt = clock.tick(fps) / 1000
+
+
 if __name__ == "__main__":
     main()
